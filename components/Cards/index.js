6aa4c22e64c79a605a5a51edf1ac643836d319e1
Stephenfre <freemanstephen37@gmail.com>
2019-09-16 03:08:23 -0700

    Components is done, but no DATA :(

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleEntry = document.querySelector(".cards-container");
+
+// makeArticleCards = async url => {
+//   try {
+//     let res = await axios.get(url);
+//     res.data.article.forEach(cards => {
+//       articleEntry.appendChild(cardContent(cards));
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// makeArticleCards("https://lambda-times-backend.herokuapp.com/articles");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(results => {
+    console.log(results);
+    results.data.articles.forEach(url => {
+      const newCard = cardContent(url);
+      articleEntry.appendChild(newCard);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function cardContent(articleInfo) {
+  const card = document.createElement("div");
+  const cardHeadLine = document.createElement("div");
+  const cardAuthor = document.createElement("div");
+  const cardImage = document.createElement("div");
+  const authorName = document.createElement("span");
+
+  // **** CREATE STRUCTURE **** //
+
+  card.appendChild(cardHeadLine);
+  card.appendChild(cardAuthor);
+  cardAuthor.appendChild(cardImage);
+  cardAuthor.appendChild(authorName);
+
+  // **** SET THE CONTENT **** //
+  cardHeadLine.textContent = articleInfo.headline;
+  authorName.textContent = articleInfo.authorName;
+  cardImage.src = articleInfo.authorPhoto;
+
+  // **** APPLY THE STYLES //
+  card.classList.add("card");
+  cardHeadLine.classList.add("headline");
+  cardAuthor.classList.add("author");
+  cardImage.classList.add("image-container");
+  // authorName.classList.add("card-author-name");
+
+  return card;
+}
